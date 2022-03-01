243310f764fc49396de02ff67f9f0e4cea30f50d
zilurahman10 <raihan.brac2019@gmail.com>
2022-03-01 16:55:12 +0600

    problem not fixed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const loadPhone = () => {
     const searchText = document.getElementById('search-field').value
+    searchText.value = '';
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
         .then(res => res.json())
@@ -9,13 +10,13 @@ const loadPhone = () => {
 const displayPhone = phones => {
     console.log(phones);
     phones.forEach(phone => {
-        console.log(phone.id);
+        console.log(phone.status);
         const showPhone = document.getElementById('show-phone')
         const div = document.createElement('div')
-        div.textContent = '';
+        phone.textContent = '';
         div.innerHTML = `
-        <div class="mb-5 rounded-3 ">
-        <div class="card" style="width: 18rem;">
+        <div class="mb-5 rounded-3 redius">
+        <div class="card  p-5 " style="width: 18rem;">
         <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">${phone.phone_name}</h5>
@@ -31,6 +32,36 @@ const displayPhone = phones => {
 
 const phoneDetails = id => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`
-    console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetails(data.data))
+}
+function displayPhoneDetails(details = 'data.data') {
+    console.log(details);
+    const showPhoneDetails = document.getElementById('phone-details')
+    const div = document.createElement('div')
+    div.innerHTML = `
+    <div class="card" style="width: 18rem;">
+        <img src="${details.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h1>${details.phone_name}</h1>
+        <p>${details.status}</p>
+            <h4>Main Features</h4>
+            <h4>Chipset : ${details.status}</h4>
+            <h4>Display Size : ${details.status}</h4>
+            <h4>Memory : ${details.status}</h4>
+
+
+            <h4>Main Features</h4>
+            <h4>Main Features</h4>
+            <h4>Main Features</h4>
+            <h4>Main Features</h4>
+            <h4>Main Features</h4>
+            <h4>Main Features</h4>
+
+        </div>
+    </div>
+    `
+    showPhoneDetails.appendChild(div)
 }
 // https://openapi.programming-hero.com/api/phone/${id}
