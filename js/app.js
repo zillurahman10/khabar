935df508baca199ceb90e6451e7fe513c1c732aa
zillurahman10 <zillur.hero40@gmail.com>
2024-05-16 19:06:13 +0600

    website-done

--- js/app.js
+++ js/app.js
@@ -1,94 +1,96 @@
-/* load Phone */
-const loadPhone = () => {
+const loadMeal = () => {
 
     const searchText = document.getElementById('search-field').value
-    /* if (typeof phones === null) {
-        console.log('No result Found')
-    } */
-    document.getElementById('phone-details').innerHTML = '';
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    document.getElementById('meal-details').innerHTML = '';
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
     fetch(url)
         .then(res => res.json())
-        .then(data => displayPhone(data.data))
+        .then(data => {
+            console.log(data.meals);
+            displayMeal(data.meals);
+        })
     document.getElementById('search-field').value = '';
 }
 
 
 
-/* show Phone */
-const displayPhone = data => {
-    const phones = data.slice(0, 20)
-    document.getElementById('show-phone').innerHTML = '';
-    const showPhone = document.getElementById('show-phone')
-    if (data.length == 0) {
-        document.getElementById('error').innerHTML = `No phone found`
+const displayMeal = data => {
+    const meals = data
+    document.getElementById('show-meal').innerHTML = '';
+    const showMeal = document.getElementById('show-meal')
+    if (data == null) {
+        document.getElementById('error').innerHTML = `No meal found`
     }
     else {
         document.getElementById('error').innerHTML = ''
-        phones.forEach(phone => {
+        meals.forEach(meal => {
             const div = document.createElement('div')
             div.textContent = '';
             div.innerHTML = `
-                        <div class="card card-style mb-5 redius-1">
+                        <div onclick='mealDetails(${meal.idMeal})' class="card card-style mb-5 redius-1">
                         <div class="p-5 ">
-                            <img src="${phone.image}" class="card-img-top" alt="...">
+                            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                             <div class="card-body">
-                                <h5 class="card-title">${phone.phone_name}</h5>
+                                <h5 class="card-title">${meal.strMeal}</h5>
                             </div>
-                            <button onclick='phoneDetails("${phone.slug}")' id="read-more" class="btn btn-outline-secondary de-flex justify-content-center">Read More</button> 
                             </div>   
                         </div>
             `
-            showPhone.appendChild(div)
+            showMeal.appendChild(div)
 
         });
     }
 }
 
 
-/* Phone Details */
-const phoneDetails = id => {
-    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+const mealDetails = id => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
     fetch(url)
         .then(res => res.json())
-        .then(data => displayPhoneDetails(data))
+        .then(data => {
+            console.log(data.meals)
+            displayMealDetails(data.meals)
+        })
 }
 
 
-/* show Phone Details  */
-const displayPhoneDetails = details => {
-    document.getElementById('phone-details').innerHTML = '';
-    const showPhoneDetails = document.getElementById('phone-details')
+const displayMealDetails = details => {
+    document.getElementById('meal-details').innerHTML = '';
+    const showmealDetails = document.getElementById('meal-details')
     const div = document.createElement('div')
-    div.innerHTML = `
-    <div class="card p-5 mb-5" style="width: 35rem;">
-        <img src="${details.data.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-        <h3>${details.data.name}</h3>
-        <p>ReleaseDate : <sapn class="text-danger">${details.data.releaseDate ? details.data.releaseDate : 'Comming soon...'}</span></p>
-            <h5>Main Features</h5>
-            <h6>Chipset : <span>${details.data.mainFeatures.chipSet}</span></h6>
-            <h6>Display Size : <span>${details.data.mainFeatures.displaySize}</span></h6>
-            <h6>Memory : <span>${details.data.mainFeatures.memory}</span></h6>
-            <h5>Sencors</h5>
-            <ul>
-                <li>${details.data.mainFeatures.sensors[0] ? details.data.mainFeatures.sensors[0] : 'Not available'}</li>
-                <li>${details.data.mainFeatures.sensors[1] ? details.data.mainFeatures.sensors[1] : 'Not available'}</li>
-                <li>${details.data.mainFeatures.sensors[2] ? details.data.mainFeatures.sensors[2] : 'Not available'}</li>
-                <li>${details.data.mainFeatures.sensors[3] ? details.data.mainFeatures.sensors[3] : 'Not available'}</li>
-                <li>${details.data.mainFeatures.sensors[4] ? details.data.mainFeatures.sensors[4] : 'Not available'}</li>
-                <li>${details.data.mainFeatures.sensors[5] ? details.data.mainFeatures.sensors[5] : 'Not available'}</li>
-            </ul>
-            <h5>Other Features</h5>
-            <h6>Bluetooth : <span>${details.data.others ? details.data.others.Bluetooth : 'Not available'}</span></h6>
-            <h6>GPS : <span>${details.data.others?.GPS ? details.data.others?.GPS : 'Not available'}</span></h6>
-            <h6>NFC : <span>${details.data.others?.NFC ? details.data.others?.NFC : 'Not available'}</span></h6>
-            <h6>Radio : <span>${details.data.others?.Radio ? details.data.others?.Radio : 'Not available'}</span></h6>
-            <h6>USB : <span>${details.data.others?.USB ? details.data.others?.USB : 'Not available'}</span></h6>
-            <h6>WLAN : <span>${details.data.others?.WLAN ? details.data.others?.WLAN : 'Not available'}</span></h6>
+    details.forEach(meal => {
+        div.innerHTML = `
+        <div class="card p-5 mb-5">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h3>${meal.strMeal}</h3>
+                <ul>
+                    <li>${meal.strIngredient1 ? meal.strIngredient1 : 'Not available'}</li>
+                    <li>${meal.strIngredient2 ? meal.strIngredient2 : 'Not available'}</li>
+                    <li>${meal.strIngredient3 ? meal.strIngredient3 : 'Not available'}</li>
+                    <li>${meal.strIngredient4 ? meal.strIngredient4 : 'Not available'}</li>
+                    <li>${meal.strIngredient5 ? meal.strIngredient5 : 'Not available'}</li>
+                    <li>${meal.strIngredient6 ? meal.strIngredient6 : 'Not available'}</li>
+                    <li>${meal.strIngredient7 ? meal.strIngredient7 : 'Not available'}</li>
+                    <li>${meal.strIngredient8 ? meal.strIngredient8 : 'Not available'}</li>
+                    <li>${meal.strIngredient9 ? meal.strIngredient9 : 'Not available'}</li>
+                    <li>${meal.strIngredient10 ? meal.strIngredient10 : 'Not available'}</li>
+                    <li>${meal.strIngredient11 ? meal.strIngredient11 : 'Not available'}</li>
+                    <li>${meal.strIngredient12 ? meal.strIngredient12 : 'Not available'}</li>
+                    <li>${meal.strIngredient13 ? meal.strIngredient13 : 'Not available'}</li>
+                    <li>${meal.strIngredient14 ? meal.strIngredient14 : 'Not available'}</li>
+                    <li>${meal.strIngredient15 ? meal.strIngredient15 : 'Not available'}</li>
+                    <li>${meal.strIngredient16 ? meal.strIngredient16 : 'Not available'}</li>
+                    <li>${meal.strIngredient17 ? meal.strIngredient17 : 'Not available'}</li>
+                    <li>${meal.strIngredient18 ? meal.strIngredient18 : 'Not available'}</li>
+                    <li>${meal.strIngredient19 ? meal.strIngredient19 : 'Not available'}</li>
+                    <li>${meal.strIngredient20 ? meal.strIngredient20 : 'Not available'}</li>
+                <ul/>
+            </div>
         </div>
-    </div>
-    `
-    showPhoneDetails.appendChild(div)
+        `
+    })
+
+    showmealDetails.appendChild(div)
 
 }
--- js/prac.js
+++
@@ -1,6 +0,0 @@
-const b = 6
-b = 6
-// console.log(b);
-
-const colors = { mango: 'green', grapes: 'black', organe: 'yellow' };
-console.log(colors[grapes])
